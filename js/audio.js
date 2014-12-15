3a2319912ca35beb26a39cfe8a96f00a930a21c8
Avril Cao <avrilcao66@gmail.com>
2014-12-15 23:48:38 +0800

    Icons hide; mp3 smaller

--- js/audio.js
+++ js/audio.js
@@ -1,25 +1,25 @@
-// In JS:
-
-var audioContainer = '#audioContainer';
+$(document).ready(function(){
+	$('.gifts').hide();
+//var audioContainer = '#audioContainer';
 var audio = '.playlist';
 var first = -1;
 var pair = false;
 
 var playlist = [
-	'a_Happy_Christmas',
-	'b_Frosty_the_Snowman',
-	'c_Santa_Baby',
-	'd_All_I_Want_for_Christmas_Is_You',
-	'e_Rockin_Around_the_Christmas_Tree',
-	'f_The_Red_Nosed_Reindeer',
-	'g_Santa_Claus_Is_Coming_to_Town'
+	'a_Happy_Christmas.mp3',
+	'b_Frosty_the_Snowman.mp3',
+	'c_Santa_Baby.mp3',
+	'd_All_I_Want_for_Christmas_Is_You.mp3',
+	'e_Rockin_Around_the_Christmas_Tree.mp3',
+	'f_The_Red_Nosed_Reindeer.mp3',
+	'g_Santa_Claus_Is_Coming_to_Town.mp3'
 ]
 
 function makeAudio(src){
-	var source = '<audio class="playlist">';
+	var source = '<audio class="playlist">' + '<source id="audio_player_mp3" src="' + src + '" type="audio/mpeg" >' + '</audio>';
 	    // source +=  '<source id="audio_player_ogv" src="' + src + '.ogv"  type="audio/ogg" />';
-	    source +=  '<source id="audio_player_mp3" src="' + src + '" type="audio/mpeg" >';
-	    source +=  '</audio>';
+	    //source +=  '<source id="audio_player_mp3" src="' + src + '" type="audio/mpeg" >';
+	    //source +=  '</audio>';
     $(audioContainer).html(source);
     var aud = $(audio).get(0);
     aud.play()
@@ -33,7 +33,7 @@ function destroyAudio(){
 $('#buttons')
 	.on('mouseenter', 'a', function(){
 		var songTitle = playlist[parseInt($(this).attr('data-song'))];
-		var src = './media/' + songTitle + '.mp3';
+		var src = './media/' + songTitle;
 		// $("audio.playlist > source").attr('src', src);
 		// var aud = $('audio.playlist').get(0);
   //   	aud.play();
@@ -44,21 +44,28 @@ $('#buttons')
 $('#buttons').on('mouseleave', 'a', destroyAudio)
 
 $('#buttons').on('click', 'a', function() {
+	var $this = $(this);
+	$(this).toggleClass('selected');
+
 	if (pair){
 		var second = parseInt($(this).attr('data-song'));
 		if (first == second){
 			console.log('Paired!')
 			// revealGifts();
+			$('a.selected').fadeOut(600);
+			var dataGift = parseInt($(this).attr('data-song'));
+			console.log(dataGift)
+			// $('#fur').show(600);
 		} else {
 			console.log('Failed!')
-			first = -1;
+			first = -1;			
 		}
 		pair = !pair;
 	} else {
 		console.log('First Selected!')
 		first = parseInt($(this).attr('data-song'));
 		pair = !pair;
-	}
-	
+	}	
+})
 
 })
